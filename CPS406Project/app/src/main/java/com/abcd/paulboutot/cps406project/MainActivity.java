1de81728436c890088fe0720ff570b803dbebe01
Joseph <jseph.tom@gmail.com>
2018-03-22 11:22:27 -0400

    Created UI elements search bar, sidebar menu

--- CPS406Project/app/src/main/java/com/abcd/paulboutot/cps406project/MainActivity.java
+++ CPS406Project/app/src/main/java/com/abcd/paulboutot/cps406project/MainActivity.java
@@ -1,13 +1,79 @@
 package com.abcd.paulboutot.cps406project;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView search_location;
+    ArrayAdapter<String> adapter;
+
+    private DrawerLayout aLayout;
+    private ActionBarDrawerToggle toggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        search_location = (ListView) findViewById(R.id.search_location);
+
+        ArrayList<String> arrayLocation = new ArrayList<>();
+        arrayLocation.addAll(Arrays.asList(getResources().getStringArray(R.array.my_locations)));
+
+        adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, arrayLocation);
+
+        search_location.setAdapter(adapter);
+
+
+
+        aLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        toggle = new ActionBarDrawerToggle(this, aLayout, R.string.open, R.string.close);
+        aLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (toggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.search_menu, menu);
+        MenuItem item = menu.findItem(R.id.search_location);
+        SearchView searchView = (SearchView)item.getActionView();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                adapter.getFilter().filter(s);
+                return false;
+            }
+        });
+
+        return super.onCreateOptionsMenu(menu);
     }
 }
